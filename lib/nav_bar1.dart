5d2f279464902bf6733a83021bdf10ce0578859d
Rishabh Sethia <rishabh@Rishabhs-MacBook-Air.local>
2021-11-16 10:06:04 +0530

    added 2 nav bars

--- lib/nav_bar1.dart
+++ lib/nav_bar1.dart
@@ -8,12 +8,52 @@ class NavBar1 extends StatefulWidget {
 }
 
 class _NavBar1State extends State<NavBar1> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Nav Bar 1'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueGrey,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        unselectedItemColor: Colors.black45,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 30,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+        ],
+      ),
+      body: _body(_selectedIndex),
     );
+  }
+
+  _body(int index) {
+    switch (index) {
+      case 0:
+        return const Text('data 1');
+      case 1:
+        return const Text('data 2');
+      case 2:
+        return const Text('data 3');
+      case 3:
+        return const Text('data 4');
+      default:
+        return const Text('data 5');
+    }
   }
 }
--- lib/nav_bar2.dart
+++ lib/nav_bar2.dart
@@ -8,10 +8,52 @@ class NavBar2 extends StatefulWidget {
 }
 
 class _NavBar2State extends State<NavBar2> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Nav Bar 2'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueGrey,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        unselectedItemColor: Colors.black45,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 30,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ''),
+        ],
+      ),
+      body: _body(_selectedIndex),
     );
+  }
+
+  _body(int index) {
+    switch (index) {
+      case 0:
+        return const Text('data 1');
+      case 1:
+        return const Text('data 2');
+      case 2:
+        return const Text('data 3');
+      case 3:
+        return const Text('data 4');
+      default:
+        return const Text('data 5');
+    }
   }
 }
